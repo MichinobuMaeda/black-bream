0d37b7cb795943fc4bbd634116efc3b73f82df5a
Michinobu Maeda <michinobumaeda@gmail.com>
2025-08-02 23:54:56 +0900

    feat: add white-bream site configuration and deployment workflows

---
+++ .github/sites/white-bream/src/firebaseConfig.js
@@ -0,0 +1,13 @@
+export const config = {
+  apiKey: "FIREBASE_API_KEY",
+  authDomain: "white-bream.web.app",
+  projectId: "white-bream",
+  storageBucket: "white-bream.firebasestorage.app",
+  messagingSenderId: "1018210955396",
+  appId: "1:1018210955396:web:7ded782d417900067c92d1",
+  measurementId: "G-3KBDG0E0WB",
+};
+
+export const reCaptchaKey = "FIREBASE_RECAPTCHA_KEY";
+
+export const region = "asia-northeast2";
